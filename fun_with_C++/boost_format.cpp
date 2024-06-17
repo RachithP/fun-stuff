6bd470b9890831c2bb96e91babf028c9a5f35fdd
rachithp <rachithprakash@gmail.com>
2024-06-16 18:23:19 -0700

    update with compile instructions

--- fun_with_C++/boost_format.cpp
+++ fun_with_C++/boost_format.cpp
@@ -1,11 +1,16 @@
+
+//
+// compile on linux
+// clang++ -std=c++20 -stdlib=libc++ -I/home/rachithp/code/github/spear/third_party/boost boost_format.cpp -Wl, -rpath /home/rachithp/softwares/llvm/llvm-project/build/lib/x86_64-unknown-linux-gnu
+//
+
 #include <boost/format.hpp>
-#include <iostream>
 #include <format>
-#include <string>
+#include <iostream>
 
 int main()
 {
-    void* ptr = new std::string("5");
+    void* ptr = new double(5);
 
     std::cout << "boost: " << (boost::format("0x%016x")%reinterpret_cast<uint64_t>(ptr)).str() << std::endl;
     std::cout << "std:   " << std::format("{:#018x}", reinterpret_cast<uint64_t>(ptr)) << std::endl;
